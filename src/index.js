09c251fa4f928e958d19ce0e9ebd3bb9d6d0e9bb
infinitey <Horng@Digitals-iMac.local>
2016-11-25 15:02:03 +0800

    multiple platform setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-ddddd
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Picker, AppState } from 'react-native';
+import Styles from './Styles.js';
+
+export default class App extends Component {
+
+  /*constructor(props) {
+    super(props);
+
+    this.state = {
+      //attrName: value,
+    }
+  }*/
+
+  render() {
+    return (
+      <View style={Styles.container}>
+        <Text>
+          Blah
+        </Text>
+      </View>
+    );
+  }
+
+}

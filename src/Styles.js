7e6d3f9f1905bf1ff2031d4f4a9900a86a7c9956
infinitey <Horng@Digitals-iMac.local>
2016-11-25 15:02:50 +0800

    styles js added

---
+++ src/Styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+  /*
+  styleReferenceName: {
+    propertyName: propertyValue/Type,
+  },
+  */
+
+});
+
+export default styles;
